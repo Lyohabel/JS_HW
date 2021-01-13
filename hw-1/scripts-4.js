9aed34d934b6a80ab7d80f20d580f2a7d62827fd
Lev <lyoha.bel2020@gmail.com>
2021-01-13 15:09:55 +0300

    finished task 6

--- hw-1/scripts-4.js
+++ hw-1/scripts-4.js
@@ -3,3 +3,6 @@ let hCil = 10;
 let VCilindra = 3.14159 * a7 / 2 * a7 / 2 * hCil + " куб. м";
 
 console.log(VCilindra);
+
+
+
---
+++ hw-1/scripts-5.js
@@ -0,0 +1,5 @@
+let r = 5;
+
+let SKruga = 3.14159 * r * r;
+
+console.log(SKruga + " кв. см");
