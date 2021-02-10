9a217d43537876d5858678092d035d787c9688e2
Lev <lyoha.bel2020@gmail.com>
2021-02-10 08:47:52 +0300

    edited hw-tabs

--- hw-tabs/scripts.js
+++ hw-tabs/scripts.js
@@ -1,37 +1,21 @@
 window.addEventListener('load', function() {
-    let tabBut1 = document.querySelector('.tab-but1'),
-        tabBut2 = document.querySelector('.tab-but2'),
-        tabBut3 = document.querySelector('.tab-but3'),
-        tabItem1 = document.querySelector('.tab-item1'),
-        tabItem2 = document.querySelector('.tab-item2'),
-        tabItem3 = document.querySelector('.tab-item3');
 
-        tabBut1.addEventListener('click', function() {
-            this.classList.add('activ');
-            tabBut2.classList.remove('activ');
-            tabBut3.classList.remove('activ');
-            tabItem1.classList.add('show');
-            tabItem2.classList.remove('show');
-            tabItem3.classList.remove('show');
-        });
-
-        tabBut2.addEventListener('click', function() {
-            this.classList.add('activ');
-            tabBut1.classList.remove('activ');
-            tabBut3.classList.remove('activ');
-            tabItem2.classList.add('show');
-            tabItem1.classList.remove('show');
-            tabItem3.classList.remove('show');
-        });
-
-        tabBut3.addEventListener('click', function() {
-            this.classList.add('activ');
-            tabBut2.classList.remove('activ');
-            tabBut1.classList.remove('activ');
-            tabItem3.classList.add('show');
-            tabItem2.classList.remove('show');
-            tabItem1.classList.remove('show');
-        });
-    
+    let tabButtons = document.querySelector('.tab-buttons'),
+        tabButs = tabButtons.children,
+        tabItems = document.querySelector('.tab-items'),
+        tabIts = tabItems.children,
+        chooseItem = function() {
+            for (let i = 0; i < tabButs.length; i++) {
+                tabButs[i].addEventListener('click', function() {
+                    for (let j = 0; j < tabButs.length; j++) {
+                        tabButs[j].classList.remove('activ');
+                        tabIts[j].classList.remove('show');
+                    }
+                    this.classList.add('activ');
+                    tabIts[i].classList.add('show');
+                });            
+            }
+        };
         
+        chooseItem();
 });
